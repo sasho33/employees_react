5e2f3d2e0ce6e1af8c93c073753c20c0909dc8ac
sasho33 <sasho3333333@gmail.com>
2023-04-28 22:23:49 +0100

    add new item

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,49 +1,48 @@
-import { Component } from 'react';
 
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      bonus: false,
-    };
+const EmployeesListItem = (props) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     bonus: false,
+  //   };
 
-    this.state = {
-      star: false,
-    };
-  }
+  //   this.state = {
+  //     star: false,
+  //   };
+  // }
   render() {
-    const { name, salary } = this.props;
-    const { bonus, star } = this.state;
+    const { name, salary, onDelete, onToggleIncrease, onToggleBonus } = this.props;
+    // const { bonus, star } = this.state;
 
     let employeeClass = 'list-group-item d-flex justify-content-between';
-    const increaseSalary = () => {
-      this.setState(({ bonus }) => ({ bonus: !bonus }));
-    };
-    if (bonus === true) {
-      employeeClass += ' increase';
-    }
+    // const increaseSalary = () => {
+    //   this.setState(({ bonus }) => ({ bonus: !bonus }));
+    // };
+    // if (bonus === true) {
+    //   employeeClass += ' increase';
+    // }
 
-    const starHandler = () => {
-      this.setState(({ star }) => ({ star: !star }));
-    };
-    if (star === true) {
-      employeeClass += ' like';
-    }
+    // const starHandler = () => {
+    //   this.setState(({ star }) => ({ star: !star }));
+    // };
+    // if (star === true) {
+    //   employeeClass += ' like';
+    // }
 
     return (
       <li className={employeeClass}>
-        <span onClick={starHandler} className="list-group-item-label">
+        <span onClick={onToggleBonus} className="list-group-item-label">
           {name}
         </span>
         <input type="text" className="list-group-item-input" defaultValue={salary + '$'} />
         <div className="d-flex justify-content-center align-items-center">
-          <button onClick={increaseSalary} type="button" className="btn-cookie btn-sm ">
+          <button onClick={onToggleIncrease} type="button" className="btn-cookie btn-sm ">
             <i className="fas fa-cookie"></i>
           </button>
           <button type="button" className="btn-trash btn-sm ">
-            <i className="fas fa-trash"></i>
+            <i className="fas fa-trash" onClick={onDelete}></i>
           </button>
           <i className="fas fa-star"></i>
         </div>
